409b840659dcc3945205c0adec3ec2ef17380b61
Mariana Libânio Barbosa <marianalibaniobarbosa@gmail.com>
2026-03-29 16:23:21 -0300

    red: adiciona testes para a função filterTasks

--- tdd-testes/tests/taskManager.test.js
+++ tdd-testes/tests/taskManager.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { removeTask } from '../src/taskManager.js';
+import { removeTask, filterTasks } from '../src/taskManager.js';
 
 describe('Função removeTask', () => {
   const tarefas = [
@@ -31,5 +31,54 @@ describe('Função removeTask', () => {
   it('Deve retornar um array vazio se a lista fornecida for vazia', () => {
     const result = removeTask([], 1);
     expect(result).toEqual([]);
+  });
+});
+
+describe('Função filterTasks', () => {
+  const tarefas = [
+    { id: 1, title: 'Estudar TDD', status: 'completed' },
+    { id: 2, title: 'Fazer commits', status: 'pending' },
+    { id: 3, title: 'Beber água', status: 'pending' }
+  ];
+
+  it('Deve retornar todas as tarefas', () => {
+    const result = filterTasks(tarefas, 'all');
+    expect(result).toHaveLength(3);
+    expect(result).toEqual(tarefas);
+  });
+
+  it('Deve retornar apenas as tarefas pendentes', () => {
+    const result = filterTasks(tarefas, 'pending');
+    expect(result).toHaveLength(2);
+    expect(result).toEqual([
+      { id: 2, title: 'Fazer commits', status: 'pending' },
+      { id: 3, title: 'Beber água', status: 'pending' }
+    ]);
+  });
+
+  it('Deve retornar apenas as tarefas concluídas', () => {
+    const result = filterTasks(tarefas, 'completed');
+    expect(result).toHaveLength(1);
+    expect(result).toEqual([
+      { id: 1, title: 'Estudar TDD', status: 'completed' }
+    ]);
+  });
+
+  it('Deve retornar todas as tarefas se o filtro for desconhecido', () => {
+    const result = filterTasks(tarefas, 'status_maluco');
+    expect(result).toHaveLength(3);
+    expect(result).toEqual(tarefas);
+  });
+
+  it('Deve retornar um array vazio se a lista fornecida for vazia', () => {
+    const result = filterTasks([], 'pending');
+    expect(result).toEqual([]);
+  });
+
+  it('Deve retornar um novo array mesmo quando retorna todas as tarefas', () => {
+    const resultAll = filterTasks(tarefas, 'all');
+    const resultDefault = filterTasks(tarefas, 'desconhecido');
+    expect(resultAll).not.toBe(tarefas);
+    expect(resultDefault).not.toBe(tarefas);
   });
 });
