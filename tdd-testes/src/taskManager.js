557836d28cf970fb046c7c106d52660ff1518d95
Mariana Libânio Barbosa <marianalibaniobarbosa@gmail.com>
2026-03-29 16:26:58 -0300

    green: implementa filterTasks usando switch case

--- tdd-testes/src/taskManager.js
+++ tdd-testes/src/taskManager.js
@@ -1,1 +1,19 @@
 export const removeTask = (tasks, taskId) => tasks.filter(task => task.id !== taskId);
+
+export function filterTasks(tasks, status) {
+  switch (status) {
+    case 'completed':
+      return tasks.filter(function(task) {
+        return task.status === 'completed';
+      });
+    case 'pending':
+      return tasks.filter(function(task) {
+        return task.status === 'pending';
+      });
+    case 'all':
+    default:
+      return tasks.filter(function() {
+        return true; 
+      });
+  }
+}
